3a4c413773f721ecee2e04f6d7e493565e5b407d
Jay Engineer <jayengineer@Jays-MacBook-Pro-2.local>
2024-04-19 18:04:36 -0400

    added bottom tabs

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-
 import { 
   View, 
   TextInput, 
@@ -11,10 +11,18 @@ import {
 } from 'react-native';
 
 import EmailBreach from "./pages/EmailBreach.js";
+import Breach from "./pages/Breach.js"
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <EmailBreach />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={EmailBreach} />
+        <Tab.Screen name="Breach" component={Breach} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
---
+++ pages/Breach.js
@@ -0,0 +1,13 @@
+import { 
+  View, 
+  TextInput, 
+  Button, 
+  Text, 
+  StyleSheet 
+} from 'react-native';
+
+const Breach = () => {
+  return (<Text>Breach</Text>)
+};
+
+export default Breach;
